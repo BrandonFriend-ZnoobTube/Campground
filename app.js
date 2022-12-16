f5dd5f169025f2eb6d4b1fdd42188633500c73a3
Beezee_Boi <beezeeboi.znoobtube@gmail.com>
2022-12-16 04:46:17 -0500

    -- Initial Commit --

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+const TryAsync = require('./utils/TryAsync');
+const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema } = require('./schemas');
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => {
+    console.log('Connected to Mongo');
+  })
+  .catch(err => {
+    console.log(`Error: ${ err }`);
+  });
+
+const app = express();
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+}
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/camp/list', TryAsync(async (req, res) => {
+  const camps = await Campground.find({});
+  res.render('camp/list', { camps });
+}));
+
+app.get('/camp/show/:id', TryAsync(async (req, res) => {
+  const camp = await Campground.findById(req.params.id);
+  res.render('camp/show', { camp });
+}));
+
+app.get('/camp/:id/edit', TryAsync(async (req, res, next) => {
+  const camp = await Campground.findById(req.params.id);
+  res.render('camp/edit', { camp });
+}));
+
+app.get('/camp/new', (req, res) => {
+  res.render('camp/new');
+});
+
+app.post('/camp/create', validateCampground, TryAsync(async (req, res, next) => {
+  const camp = new Campground(req.body.campground);
+  await camp.save();
+  res.redirect(`/camp/show/${camp._id}`);
+}));
+
+app.put('/camp/:id', validateCampground, TryAsync(async (req, res) => {
+  const { id } = req.params;
+  const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+  res.redirect(`/camp/show/${camp._id}`);
+}));
+
+app.delete('/camp/delete/:id', TryAsync(async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/camp/list');
+}));
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500} = err;
+  if (!err.message) { err.message = 'Something went wrong'; }
+  res.status(statusCode).render('error', { err });
+});
+
+app.listen(3000, () => {
+  console.log('Running on port 3000');
+});
---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { descriptors, places } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => {
+    console.log('Connected to Mongo');
+  })
+  .catch(err => {
+    console.log(`Error: ${ err }`);
+  });
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' +
+        'Ut faucibus pulvinar elementum integer enim neque volutpat. Venenatis lectus magna fringilla urna porttitor rhoncus dolor. ' +
+        'Donec pretium vulputate sapien nec sagittis aliquam malesuada. Aliquam sem fringilla ut morbi.',
+      price
+    })
+    await camp.save();
+  }
+}
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
