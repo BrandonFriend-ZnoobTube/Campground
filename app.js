c790d19243665a24b22c914c9fa5508f8cd325c6
Beezee_Boi <beezeeboi.znoobtube@gmail.com>
2022-12-21 12:15:38 -0500

    add session secret

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
-  secret: 'thisismysecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
